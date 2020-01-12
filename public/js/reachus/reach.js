828bb692a9927cfa3d0ae4f6687c7fe950ed4cf2
Prakash Jha <only2prakash@gmail.com>
2020-01-12 10:21:46 +0530

    updated reachus

--- public/js/reachus/reach.js
+++ public/js/reachus/reach.js
@@ -1,38 +1,39 @@
-var bx=document.getElementById('carj');
-var sx=document.getElementById('sign1j');
-var tx=document.getElementById('trainj');
-var i=0;
-var j=0;
-function fun() {
-const scrolled= window.scrollY;
-console.log(scrolled);
-if (scrolled>500) {
-	bx.className='car';
-	sx.className='sign1'
-	
+var bx = document.getElementById('carj');
+var sx = document.getElementById('sign1j');
+var tx = document.getElementById('trainj');
+var i = 0;
+var j = 0;
 
-	i+=1;
-}else if(scrolled<500 && i>=1) {
-	bx.className="carj";
-	sx.className="sign1r"
-	
-}
+function fun() {
+    const scrolled = window.scrollY;
+    // console.log(scrolled);
+    if (scrolled > 500) {
+        bx.className = 'car';
+        sx.className = 'sign1'
+
+
+        i += 1;
+    } else if (scrolled < 500 && i >= 1) {
+        bx.className = "carj";
+        sx.className = "sign1r"
+
+    }
 }
 
 // function funt() {
 // const scrolled= window.scrollY;
 // console.log(scrolled);
 // if (scrolled>400) {
-	
+
 // 	tx.className='train'
 
 // 	j+=1;
 // }else if(scrolled<400 && j>=1) {
-	
+
 // 	tx.className='trainj'
 // }
 // }
 
 
-window.addEventListener('scroll',fun);
+window.addEventListener('scroll', fun);
 // window.addEventListener('scroll',funt);
