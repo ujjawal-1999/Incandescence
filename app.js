014c0cdf8a030a4e8b099c36b1d0a4c2272486e7
Tarun Singhania <37648118+TarunSinghania@users.noreply.github.com>
2020-01-15 11:14:42 +0530

    update app.js for nginx
    use node local server to serve requests for nginx

--- app.js
+++ app.js
@@ -30,11 +30,12 @@ var app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 //--- http server code  begins--
+/*
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(5000);
 // // ends
-
+*/
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //Seeding
@@ -286,6 +287,6 @@ app.post('/check', (req, res) => {
 // End of post request
 
 
-// app.listen(5000, () => {
-    // console.log('Connected to server')
-// })
+ app.listen(5000, () => {
+     console.log('Connected to server')
+ })
