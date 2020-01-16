894a93608b8706d8bd4e68a56f88f0aa430681ac
nabanitabania <nabanitabania78@gmail.com>
2020-01-16 21:43:36 +0530

    campus

--- app.js
+++ app.js
@@ -289,6 +289,7 @@ app.post('/check', (req, res) => {
 // app.listen(5000, () => {
     // console.log('Connected to server')
 // })
+<<<<<<< HEAD
 
 
 
@@ -299,3 +300,5 @@ app.post('/check', (req, res) => {
 
 
 
+=======
+>>>>>>> done
