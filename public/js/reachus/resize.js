08b041e35676e8fb19a7345473612a56bd1274a0
forgotter <only2prakash@gmail.com>
2020-01-18 01:36:11 +0530

    Fixed ReachUs placement bug

--- public/js/reachus/resize.js
+++
@@ -1,7 +0,0 @@
-function resizer() {
-    let intViewportHeight = window.innerHeight;
-    var road = parseFloat(document.getElementById('road-container').clientHeight);
-    console.log(road);
-    document.getElementById('map').style.top = road + 0.85 * intViewportHeight + 'px';
-    document.getElementById('footer').style.top = road + 0.85 * intViewportHeight + 300 + 'px';
-}
