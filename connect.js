3a51dbcf74fd00976ff79e9a1ea094afc516353f
Abinash Dutta <abinashdtt45@gmail.com>
2020-01-19 19:52:53 +0530

    competition database made

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/User',{ useNewUrlParser: true, useUnifiedTopology: true,
+mongoose.connect('mongodb://localhost:27017/Incandescence',{ useNewUrlParser: true, useUnifiedTopology: true,
 useCreateIndex: true })
 module.exports={mongoose}
 
