6fb413109645706fa2d04214108b866b44fabde3
Tarun Singhania <37648118+TarunSinghania@users.noreply.github.com>
2020-01-22 17:32:31 +0530

    app.js reolve 502 bad gateway

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ var app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
     //--- http server code  begins--
-var http = require('http');
-var httpServer = http.createServer(app);
-httpServer.listen(80);
+//var http = require('http');
+//var httpServer = http.createServer(app);
+//httpServer.listen(80);
 // // ends
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -403,9 +403,7 @@ app.post('/check', (req, res) => {
 // End of post request
 
 
-
-
- // app.listen(5000, () => {
- //     console.log('Connected to server')
- // })
+app.listen(5000, () => {
+      console.log('Connected to server')
+  })
 
