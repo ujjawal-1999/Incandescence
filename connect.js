376f72b6f9b417e53a27a9731e9c91ec7f52c5df
rayabhisek22 <rayabhisek22@gmail.com>
2020-01-27 17:22:54 +0530

    Removed the backend part completely

--- connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/Incandescence',{ useNewUrlParser: true, useUnifiedTopology: true,
-useCreateIndex: true })
-module.exports={mongoose}
-
-
